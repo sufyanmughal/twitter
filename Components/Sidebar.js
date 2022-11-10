6f84392b4d50d505943d021c4e74a783e8b9f15b
Abu sufyan <captainsufyan158005@gmail.com>
2022-11-10 20:39:46 +0500

    completion of sidebar completed

--- Components/Sidebar.js
+++ Components/Sidebar.js
@@ -14,11 +14,11 @@ export default function Sidebar() {
   return (
     <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
         {/*Twitter Logo*/}
-        <div className="hoverEffect">
+        <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
         <Image src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png" width="50" height="50"></Image>
         </div>
          {/*Menu*/}
-         <div>
+        <div className="mt-4 mb-2.5 xl:items-start">
         <SidebarMenuitem text="Home" Icon={HomeIcon} active/>
         <SidebarMenuitem text="Explore" Icon={HashtagIcon}/>
         <SidebarMenuitem text="Notification" Icon={BellIcon}/>
@@ -30,12 +30,12 @@ export default function Sidebar() {
         </div>
         <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">Tweet</button>
         <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
-            <Image width={50} height={50} alt="userimage" src={"https://www.upwork.com/profile-portraits/c1GwQzOgWR4bnBto0ssa6my1CYrQq2Dz8wFZN-XnUDUX50AM0aa99UvP-FAxUP16FD"}></Image>
-            <div>
-                <h4>Sufyan ahmad</h4>
-                <p>@sufyanmughal</p>
+            <Image className="w-10 h-10 rounded-full xl:mr-2" width={50} height={50} alt="userimage" src={"https://www.upwork.com/profile-portraits/c1GwQzOgWR4bnBto0ssa6my1CYrQq2Dz8wFZN-XnUDUX50AM0aa99UvP-FAxUP16FD"}></Image>
+            <div className="leading-5">
+                <h4 className="font-bold">Sufyan ahmad</h4>
+                <p className="text-gray-500">@sufyanmughal</p>
             </div>
-            <DotsHorizontalIcon className="h-5"  />
+            <DotsHorizontalIcon className="h-5 xl:ml-8"  />
         </div>
        
         {/*Button*/}
